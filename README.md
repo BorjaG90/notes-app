6230a4851c24c8efa13c05995cb810535cafb0f9
Borja Gete <borjagete90@outlook.es>
2018-11-27 11:37:52 +0000

    Add Note model

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ npm install
 ### Backend
 <a href="https://www.javascript.com/"><img src="https://raw.githubusercontent.com/BorjaG90/media/master/img/logos/javascript.jpeg" width=50 alt="JavaScript"></a>
 <a href="https://nodejs.org/es/"><img src="https://raw.githubusercontent.com/BorjaG90/media/master/img/logos/nodejs.png" width=50 alt="NodeJS"></a>
+### Frontend
+<a href="https://www.getbootstrap.com/"><img src="https://raw.githubusercontent.com/BorjaG90/media/master/img/logos/bootstrap.png" width=50 alt="BootStrap4"></a>
 ### Platforms
 <a href="https://aws.amazon.com/es/cloud9/"><img src="https://raw.githubusercontent.com/BorjaG90/media/master/img/logos/c9io.png" width=50 alt="Cloud9"></a>
 <a href="https://code.visualstudio.com/"><img src="https://raw.githubusercontent.com/BorjaG90/media/master/img/logos/vscode.png" width=50 alt="VSCode"></a>
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
 
+const Note = require("../models/Note");
+
 router.get('/notes/add', (req, res) =>{
-    res.render('notes/new-note');
+  res.render('notes/new-note');
 });
 
 router.post("/notes/new-note", (req, res) => {
-    console.log(req.body);
-    res.send("OK");
+  const {title, description} = req.body;
+  const errors = [];
+  if(!title){
+    errors.push({text: 'Please Write a Title'});
+  }
+  if(!description){
+    errors.push({text: 'Please Write a Description'});
+  }
+  if(errors.length > 0){
+    res.render('notes/new-note', {
+      errors,
+      title,
+      description
+    });
+  } else {
+    const newNote = new Note({title, description});
+    console.log(newNote);
+    res.send('ok');
+  }
 });
 
 router.get('/notes', (req, res) => {
-    res.send('Notas desde la BD');
+  res.send('Notas desde la BD');
 });
 
 module.exports = router;
