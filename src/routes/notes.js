4004dac5b22a599a3a071d6057428c2e3a6bd1f0
Borja Gete <borjagete90@outlook.es>
2018-11-27 12:57:50 +0000

    Add edit notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -23,7 +23,7 @@ router.post("/notes/new-note", async (req, res) => {
     });
   } else {
     const newNote = new Note({ title, description });
-    await newNote.save(); // Procesos sincronos
+    await newNote.save(); // Procesos asincronos
     res.redirect('/notes');
   }
 });
@@ -33,5 +33,15 @@ router.get('/notes', async (req, res) => {
   res.render('notes/all-notes', { notes });
 });
 
+router.get('/notes/edit/:id', async (req, res) =>{
+  const note = await Note.findById(req.params.id);
+  res.render('notes/edit-note', {note})
+});
+
+router.put('/notes/edit-note/:id', async (req, res) =>{
+  const { title, description } = req.body;
+  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  res.redirect('/notes');
+});
 
 module.exports = router;
