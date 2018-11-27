9d53fe23a6dc6d01910b28a48a6a080e82757439
Borja Gete <borjagete90@outlook.es>
2018-11-27 11:18:20 +0000

    Add form new notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+router.get('/notes/add', (req, res) =>{
+    res.render('notes/new-note');
+});
+
+router.post("/notes/new-note", (req, res) => {
+    console.log(req.body);
+    res.send("OK");
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notas desde la BD');
 });
