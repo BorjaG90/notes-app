ecb88bb6aadc1ef0c093252f225c8afc8d6f77b9
Borja Gete <borjagete90@outlook.es>
2018-11-27 12:33:27 +0000

    Add show notes from database

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,7 +6,7 @@ router.get('/notes/add', (req, res) =>{
   res.render('notes/new-note');
 });
 
-router.post("/notes/new-note", (req, res) => {
+router.post("/notes/new-note", async (req, res) => {
   const {title, description} = req.body;
   const errors = [];
   if(!title){
@@ -22,14 +22,16 @@ router.post("/notes/new-note", (req, res) => {
       description
     });
   } else {
-    const newNote = new Note({title, description});
-    console.log(newNote);
-    res.send('ok');
+    const newNote = new Note({ title, description });
+    await newNote.save(); // Procesos sincronos
+    res.redirect('/notes');
   }
 });
 
-router.get('/notes', (req, res) => {
-  res.send('Notas desde la BD');
+router.get('/notes', async (req, res) => {
+  const notes = await Note.find().sort({date: "desc"});
+  res.render('notes/all-notes', { notes });
 });
+
 
 module.exports = router;
