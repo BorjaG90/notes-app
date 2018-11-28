a656d53363fb34d0a77f63443ca2fcb81c368acc
Borja Gete <borjagete90@outlook.es>
2018-11-28 10:34:25 +0000

    Add User form and cypher passwords

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
 
 router.get('/signin', (req, res) => {
-    res.render('users/signin');
+  res.render('users/signin');
 });
 
 router.get('/signup', (req, res) => {
-    res.render('users/signup');
+  res.render('users/signup');
 });
 
+router.post('/users/signup', (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
+  const errors = [];
+  
+  if(name.length <= 0){
+    errors.push({text: "Please insert your name"});
+  }
+  if(password != confirm_password){
+    errors.push({text: "Password do not match"});
+  }
+  if(password.length<4){
+    errors.push({text: "Password must be at least 4 characteres"});
+  }
+  
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, password, confirm_password});
+  } else {
+    res.send('Ok');
+  }
+  
+});
 module.exports = router;
