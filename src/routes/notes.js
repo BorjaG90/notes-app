5239cb39ea3b60840b29bc455860bcd70276f974
Borja Gete <borjagete90@outlook.es>
2018-11-30 07:52:54 +0000

    Filter views by user's permission

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -24,6 +24,7 @@ router.post("/notes/new-note", isAuthenticated,  async (req, res) => {
     });
   } else {
     const newNote = new Note({ title, description });
+    newNote.user = req.user.id;
     await newNote.save(); // Procesos asincronos
     req.flash('success_msg','Note Added Successfully');
     res.redirect('/notes');
@@ -31,7 +32,7 @@ router.post("/notes/new-note", isAuthenticated,  async (req, res) => {
 });
 
 router.get('/notes', isAuthenticated,  async (req, res) => {
-  const notes = await Note.find().sort({date: "desc"});
+  const notes = await Note.find({user: req.user.id}).sort({date: "desc"});
   res.render('notes/all-notes', { notes });
 });
 
